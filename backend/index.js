6b0bb48403a4215b0e574146cfac25038c01cc61
edaywalid <becharwalid9@gmail.com>
2024-06-04 17:13:38 +0100

    backend : added hello-world endpoint

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const authRoutes = require("./routes/auth");
+const helloWorldRoutes = require("./routes/hello.js");
 const authMiddleware = require("./middleware/auth");
 const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
@@ -50,6 +51,7 @@ mongoose
   .catch((err) => console.error(err));
 
 app.use(`/${process.env.HOST_URL_ENDPOINTS}/auth`, authRoutes);
+app.use(`/${process.env.HOST_URL_ENDPOINTS}`, helloWorldRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
