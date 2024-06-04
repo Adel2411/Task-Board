82afda436fc2a9b5e3b9ed4d08bd5cd671c24ba1
edaywalid <becharwalid9@gmail.com>
2024-06-04 10:38:40 +0100

    backend : convert the type to commonjs ; add tasks array to BoardSchema to make it easy when fetching board's tasks

--- backend/models/Board.js
+++ backend/models/Board.js
@@ -1,14 +1,15 @@
-import mongoose from "mongoose";
-import { v4 as uuidv4 } from "uuid";
-
+const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
+const taskSchema = require('./Task');
 
 const boardSchema = new mongoose.Schema({
   _id: { type: String, default: uuidv4 }, // Setting _id to use UUID
   name: { type: String, required: true },
   description: { type: String, required: false },
   owner: { type: String, ref: 'User', required: true }, // Correcting ref syntax
-  is_public: { type: Boolean, default: false } // Simplifying the default value placement
+  is_public: { type: Boolean, default: false }, // Simplifying the default value placement
+  tasks: []
 });
 
-export default mongoose.model('Board', boardSchema);
+module.exports = mongoose.model('Board', boardSchema);
 
