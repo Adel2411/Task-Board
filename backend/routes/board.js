3c58ee4fb6fee669ecc010c0ff166980141382ad
edaywalid <becharwalid9@gmail.com>
2024-06-05 21:02:34 +0100

    backend : added update board route

--- backend/routes/board.js
+++ backend/routes/board.js
@@ -31,4 +31,24 @@ router.get("/getAll", authMiddleware, async (req, res) => {
   }
 });
 
+// Update board
+router.put("/update/:id", authMiddleware, async (req, res) => {
+  const boardId = req.params.id;
+  const { name, description, is_public } = req.body;
+  try {
+    const board = await Board.findById(boardId);
+    if (!board) return res.status(404).json({ message: "Board not found" });
+    if (board.owner !== req.user.id) {
+      return res.status(403).json({ message: "Unauthorized access" });
+    }
+    const updatedBoard = await Board.findByIdAndUpdate(
+      boardId,
+      { name, description, is_public },
+      { new: true },
+    );
+    res.json(updatedBoard);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 module.exports = router;
