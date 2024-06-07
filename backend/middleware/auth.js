07cf6fb0d544e5f79da0733e2791eff3cb628be4
edaywalid <becharwalid9@gmail.com>
2024-06-07 16:40:32 +0100

    backend : make the req.user the whole user instead of the jwt payload

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,17 +1,18 @@
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
+const User = require('../models/User');
 
 dotenv.config();
 
-const auth = (req, res, next) => {
+const auth = async (req, res, next) => {
   const token = req.header('Authorization').replace('Bearer ', '');
   if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = findUserById(decoded.id);
+    const user = await User.findById(decoded._id);
     if (!user) return res.status(401).json({ message: 'Token is not valid' });
-    req.user = decoded;
+    req.user = user;
     next();
   } catch (err) {
     res.status(401).json({ message: 'Token is not valid' });
