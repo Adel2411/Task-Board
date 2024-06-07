5ca784940f3c3a618ba3d92fdce65af516548a85
edaywalid <becharwalid9@gmail.com>
2024-06-07 16:42:57 +0100

    backend : added api docs for user route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,31 @@ const express = require("express");
 const authMiddleware = require("../middleware/auth");
 
 const router = express.Router();
+/**
+ * @swagger
+ * /user-data:
+ *   get:
+ *     summary: Retrieve user data
+ *     description: Retrieve the username and email of the authenticated user.
+ *     tags:
+ *       - User
+ *     responses:
+ *       200:
+ *         description: A JSON object containing the user's username and email.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 username:
+ *                   type: string
+ *                   example: johndoe
+ *                 email:
+ *                   type: string
+ *                   example: johndoe@example.com
+ *       401:
+ *         description: Unauthorized
+ */
 
 router.get("/user-data", authMiddleware, (req, res) => {
   res.json({
