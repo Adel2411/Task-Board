8e4102dd1c3c9543bae6e1bdd9516dbf8aa477f1
edaywalid <becharwalid9@gmail.com>
2024-06-08 22:39:32 +0100

    backend : added delete task endpoint

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const Task = require("../models/Task");
+const authMiddleware = require("../middleware/auth");
 
-const Router = express.Router();
+const router = express.Router();
 
-module.exports = Router;
+router.delete("/:id", authMiddleware, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const task = await Task.findById(id);
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    if (task.owner.toString() !== req.user._id) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+    await Task.findByIdAndDelete(id);
+    res.json({ message: "Task deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+module.exports = router;
