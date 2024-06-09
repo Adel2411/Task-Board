a86e3af4f31b00f182622fb4f5e353beab7f9f91
edaywalid <becharwalid9@gmail.com>
2024-06-09 23:51:07 +0100

    backend : added update-task endpoint

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -38,4 +38,29 @@ router.post("/add", authMiddleware, async (req, res) => {
   }
 });
 
+router.put("/update/:id", authMiddleware, async (req, res) => {
+  const { id } = req.params;
+  const { title, description, taskIcon, status } = req.body;
+  try {
+    const task = await Task.findById(id);
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    const containerBoard = await Board.findById(task.board);
+    if (containerBoard.owner.toString() !== req.user._id) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+    task.title = title;
+    task.description = description;
+    task.taskIcon = taskIcon;
+    task.status = status;
+
+    await task.save();
+
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 module.exports = router;
