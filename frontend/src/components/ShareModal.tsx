793894d3eb5c479070b1fd8eaba2d6472354d34b
Adel2411 <hadjarabadel.2411@gmail.com>
2024-06-14 15:28:28 +0100

    frontend : Changed the shareModal

--- frontend/src/components/ShareModal.tsx
+++ frontend/src/components/ShareModal.tsx
@@ -3,6 +3,8 @@
 import ReactDOM from "react-dom";
 import { shareModalProps } from "@/types";
 import { motion, AnimatePresence } from "framer-motion";
+import { IoClose } from "react-icons/io5";
+import { buttonVariants } from "@/animations";
 
 const ShareModal = ({ isOpen, closeModal, link }: shareModalProps) => {
   const backdropVariants = {
@@ -20,6 +22,10 @@ const ShareModal = ({ isOpen, closeModal, link }: shareModalProps) => {
     exit: { scale: 0, transition: { duration: 1, type: "spring" } },
   };
 
+  const handleCopy = () => {
+    navigator.clipboard.writeText(link);
+  };
+
   const modalContent = (
     <AnimatePresence>
       {isOpen && (
@@ -33,12 +39,31 @@ const ShareModal = ({ isOpen, closeModal, link }: shareModalProps) => {
             onClick={closeModal}
           />
           <motion.div
-            className={`z-10 flex flex-col justify-center gap-8 bg-background dark:bg-background-dark rounded-lg p-6 sm:w-2/3 md:w-1/2 lg:w-1/3 shadow-lg`}
+            className={`relative z-10 flex flex-col justify-center gap-8 bg-background dark:bg-background-dark rounded-lg p-6 sm:w-2/3 md:w-1/2 lg:w-1/3 shadow-lg`}
             initial="hidden"
             animate="visible"
             exit="exit"
             variants={modalVariants}
-          ></motion.div>
+          >
+            <motion.button
+              initial={{ opacity: 0.5 }}
+              whileHover={{ opacity: 1, transition: { duration: 0.1 } }}
+              className="absolute top-2 right-2 disabled:opacity-30"
+              onClick={closeModal}
+            >
+              <IoClose size={20} className="text-black dark:text-white" />
+            </motion.button>
+            <p>{link}</p>
+            <motion.button
+              variants={buttonVariants}
+              whileHover="hover"
+              whileTap="tap"
+              onClick={handleCopy}
+              className="disabled:cursor-not-allowed disabled:opacity-40 text-sm px-4 py-2 bg-primary dark:bg-primary-dark text-white rounded-md"
+            >
+              Copy
+            </motion.button>
+          </motion.div>
         </div>
       )}
     </AnimatePresence>
