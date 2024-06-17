158dc9a28e0df475109932b2aab17878f1e67d17
Adel2411 <hadjarabadel.2411@gmail.com>
2024-06-17 01:58:34 +0100

    frontend: Share link modal finished

--- frontend/src/components/ShareModal.tsx
+++ frontend/src/components/ShareModal.tsx
@@ -4,9 +4,15 @@ import ReactDOM from "react-dom";
 import { shareModalProps } from "@/types";
 import { motion, AnimatePresence } from "framer-motion";
 import { IoClose } from "react-icons/io5";
+import { HiClipboardDocument, HiClipboardDocumentCheck } from "react-icons/hi2";
 import { buttonVariants } from "@/animations";
+import { useState } from "react";
+import Toast from "./Toast";
+import toast from "react-hot-toast";
 
 const ShareModal = ({ isOpen, closeModal, link }: shareModalProps) => {
+  const [isCopied, setIsCopied] = useState(false);
+
   const backdropVariants = {
     hidden: { opacity: 0 },
     visible: { opacity: 0.8 },
@@ -23,7 +29,18 @@ const ShareModal = ({ isOpen, closeModal, link }: shareModalProps) => {
   };
 
   const handleCopy = () => {
+    setIsCopied(true);
     navigator.clipboard.writeText(link);
+    toast.custom((t) => (
+      <Toast
+        message="Link copied to clipboard, you can now share it with your friends !"
+        type="success"
+        t={t}
+      />
+    ));
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 3000);
   };
 
   const modalContent = (
@@ -39,7 +56,7 @@ const ShareModal = ({ isOpen, closeModal, link }: shareModalProps) => {
             onClick={closeModal}
           />
           <motion.div
-            className={`relative z-10 flex flex-col justify-center gap-8 bg-background dark:bg-background-dark rounded-lg p-6 sm:w-2/3 md:w-1/2 lg:w-1/3 shadow-lg`}
+            className={`relative z-10 flex flex-col justify-center gap-5 bg-background dark:bg-background-dark rounded-lg pt-10 p-6 sm:w-2/3 md:w-1/2 lg:w-1/3 shadow-lg`}
             initial="hidden"
             animate="visible"
             exit="exit"
@@ -53,16 +70,26 @@ const ShareModal = ({ isOpen, closeModal, link }: shareModalProps) => {
             >
               <IoClose size={20} className="text-black dark:text-white" />
             </motion.button>
-            <p>{link}</p>
-            <motion.button
-              variants={buttonVariants}
-              whileHover="hover"
-              whileTap="tap"
-              onClick={handleCopy}
-              className="disabled:cursor-not-allowed disabled:opacity-40 text-sm px-4 py-2 bg-primary dark:bg-primary-dark text-white rounded-md"
-            >
-              Copy
-            </motion.button>
+            <h1 className="text-xl md:text-2xl font-semibold">Share Link</h1>
+            <div className="flex items-center justify-start">
+              <input
+                value={link}
+                className="w-full px-4 h-10 bg-black bg-opacity-5 dark:bg-white dark:bg-opacity-5 rounded-l-md"
+              />
+              <motion.button
+                variants={buttonVariants}
+                whileHover={isCopied ? {} : "hover"}
+                onClick={handleCopy}
+                disabled={isCopied}
+                className="h-10 flex items-center justify-center gap-2 disabled:cursor-default disabled:bg-opacity-60 dark:disabled:bg-opacity-60 text-sm px-4 py-2 bg-primary dark:bg-primary-dark text-white rounded-r-md"
+              >
+                {isCopied ? (
+                  <HiClipboardDocumentCheck size={20} />
+                ) : (
+                  <HiClipboardDocument size={20} />
+                )}
+              </motion.button>
+            </div>
           </motion.div>
         </div>
       )}
