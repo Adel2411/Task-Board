a5818066bd1533fed480d91b7150faa72720aaea
edaywalid <becharwalid9@gmail.com>
2024-06-18 23:12:27 +0100

    backend : changed the status _id to id

--- backend/models/Status.js
+++ backend/models/Status.js
@@ -1,9 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const statusSchema = new mongoose.Schema({
-  _id: { type: Number, required: true, unique: true },
-  name: { type: String, required: true }
+  id: { type: Number, required: true, unique: true },
+  name: { type: String, required: true },
 });
 
-module.exports = mongoose.model('Status', statusSchema);
-
+module.exports = mongoose.model("Status", statusSchema);
