274fbef964c2ffb2365591e00544623f83bd335b
Adel2411 <hadjarabadel.2411@gmail.com>
2025-01-01 15:18:22 +0100

    Finished Hero Section

--- frontend/src/components/landing-page/Hero.tsx
+++ frontend/src/components/landing-page/Hero.tsx
@@ -2,6 +2,8 @@
 
 import { motion, useMotionValue, useTransform } from "framer-motion";
 import Image from "next/image";
+import GradientDiv from "../GradientDiv";
+import Link from "next/link";
 
 export default function Hero() {
   const x = useMotionValue(0);
@@ -23,24 +25,28 @@ export default function Hero() {
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col md:flex-row items-center">
         <motion.div
-          className="md:w-1/2 text-center md:text-left mb-12 md:mb-0"
+          className="md:w-1/2 text-center md:text-left flex flex-col items-center md:items-start justify-center"
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
         >
-          <h1 className="text-4xl md:text-6xl font-bold mb-6 text-foreground">
-            Your Tasks, <span className="text-primary">Your Way</span>
+          <h1 className="text-4xl mb-4 md:text-6xl font-bold text-foreground">
+            Your Tasks,{" "}
+            <span className="text-primary dark:text-primary-dark">
+              Your Way
+            </span>
           </h1>
           <p className="text-xl mb-8 text-foreground/80">
             Organize your boards, manage tasks, and share progress effortlessly
           </p>
-          <motion.button
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-            className="bg-primary text-primary-foreground px-8 py-3 rounded-full text-lg font-semibold shadow-lg hover:shadow-xl transition-shadow duration-200"
-          >
-            Get Started
-          </motion.button>
+          <GradientDiv className="rounded-xl p-[3px] w-fit">
+            <Link
+              className="btn btn-ghost bg-white hover:bg-white/80 dark:bg-black dark:hover:bg-black/80 hover:text-primary dark:hover:text-primary-dark"
+              href="/auth"
+            >
+              Get Started
+            </Link>
+          </GradientDiv>
         </motion.div>
         <motion.div
           className="md:w-1/2"
